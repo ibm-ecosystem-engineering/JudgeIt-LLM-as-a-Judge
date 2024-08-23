343dbd9e376e2d165dd0ac8cd018901dbd3367e7
Himadri Talukder <htalukder@ibm.com>
2024-08-23 18:06:08 -0400

    updated config

--- JudgeIt App/services/Config.js
+++ JudgeIt App/services/Config.js
@@ -1,4 +1,4 @@
-export const LLM_JUDGE_BASE_URL = process.env.NEXT_PUBLIC_JUDGE_BACKEND_URL;
+export const LLM_JUDGE_BASE_URL = process.env.NEXT_PUBLIC_JUDGE_BACKEND_URL || process.env.JUDGE_BACKEND_URL;
 export const LLM_JUDGE_BATCH_EVENT_URL = LLM_JUDGE_BASE_URL + "/api/v1/judge/events/";
 export const LLM_JUDGE_DOWNLOAD_EVALUATION_URL = LLM_JUDGE_BASE_URL + "/api/v1/judge/download/"
 
@@ -7,7 +7,7 @@ export const API_TYPE_RATING = "rating";
 export const API_TYPE_SIMILARITY = "similarity";
 export const API_TYPE_MULTITURN = "multiturn";
 
-export const LLM_JUDGE_API_KEY_SECRET=process.env.NEXT_PUBLIC_LLM_JUDGE_API_KEY;
+export const LLM_JUDGE_API_KEY_SECRET=process.env.NEXT_PUBLIC_LLM_JUDGE_API_KEY || process.env.LLM_JUDGE_API_KEY;
 
 export const LLM_MODELS = [
     /*
